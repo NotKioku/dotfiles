f7443648e04bdce370a620db354eefa575c5b1f2
Jun <cjy01761540@gmail.com>
2026-05-11 19:17:47 +0800

    add komorebi whkd env and start alias

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,9 +2,12 @@ Set-PSReadLineOption -PredictionSource None
 
 if (Get-Alias ls -ErrorAction SilentlyContinue) { Remove-Item alias:ls }
 function ls { eza --icons=auto --group-directories-first $args }
+function ks { komorebic start --whkd }
 
 Invoke-Expression (&starship init powershell)
 
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+$ENV:KOMOREBI_CONFIG_HOME = "$HOME\.config\komorebi"
+$ENV:WHKD_CONFIG_HOME = "$HOME\.config\whkd"
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
